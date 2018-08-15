1280050b7f4e3a4e3629dddd3a938b5d184e093b
hanshantong <1356466973@qq.com>
2018-08-15 17:40:46 +0800

    Fib.py and Stdutent.py modified.

--- Fib.py
+++ Fib.py
@@ -6,6 +6,7 @@ __author__ = "tongzi"
 '''
 斐波那契数列
 '''
+import logging
 
 class Fib:
 	def __init__(self):
@@ -21,10 +22,43 @@ class Fib:
 		if self.a > 10000:  #退出循环的条件
 			raise StopIteration()
 		return self.a
-	
+		
+	def __getitem__(self,n):
+		if(isinstance(n,int)):
+			a,b = 1,1
+			for x in range(n):
+				a,b = b,a+b
+			return a
+		if(isinstance(n,slice)):
+			start = n.start
+			stop = n.stop
+			if start is None:
+				start = 0
+			a, b = 1, 1
+			L = []
+			for x in range(stop):
+				if x >= start:
+					L.append(a)
+				a, b = b, a + b
+			return L
+		
+		
+		
 if __name__ == "__main__":
 	for n in Fib():
 		print(n)
+	try:
+		n = 0
+		assert n!=0,'n is zero!'
+		r = 10 / n
+		print(r)
+	except ZeroDivisionError as e:
+		#print("Exception:",e)
+		logging.exception(e)
+	else:
+		print("No Exception!")
+	finally:
+		print("Release resource..")
 		
 		
 		
--- Student.py
+++ Student.py
@@ -7,8 +7,10 @@ Student module.
 
 __author__ = 'tongzi'
 
+from enum import Enum
+
 class Person(object):
-	__slots__ = ('name','age','title')
+	#__slots__ = ('name','age','title')
 	def work(self):
 		print("Person is working...")
 
@@ -16,9 +18,10 @@ class Person(object):
 
 class Student(Person):
 	#__slots__ = ('name','age','title')
-	def __init__(self,name,age):
+	def __init__(self,name,age,gender):
 		self.name = name
 		self.age = age
+		self._gender = gender
 		
 	def print_info(self):
 		print("name: %s, age: %s"%(self.name,self.age))
@@ -29,19 +32,37 @@ class Student(Person):
 		
 	@property
 	def gender(self):
-		return self.gender
+		return self._gender
 		
 	@gender.setter
 	def gender(self,val):
-		self.gender = val
+		self._gender = val
 		
 	def __str__(self):	
-		return ("Student object-->> name: %s, age: %s"%(self.name,self.age))
+		return ("Student object-->> name: %s, age: %s, gender: %s"%(self.name,self.age,self._gender))
 	__repr__ = __str__	
-		
-		
+	
+	def __getattr__(self, attr):
+		if attr == "score":
+			return 100
+		return None
+	def __call__(self):
+		print("Student->> name is %s, age is %s"%(self.name,self.age))
+	
+def student2dict(s):
+	return {'name':s.name,'age':s.age,'gender':s._gender}
+	
+def dict2student(d):
+	return Student(d['name'],d['age'],d['gender'])
+
+	
 		
 class Teacher(Person):
+	def __init__(self,name,age,title):
+		self.name = name
+		self.age = age
+		self.title = title
+
 	def work(self):
 		print("Teacher is teaching...")
 
@@ -53,14 +74,20 @@ def whowork(person):
 
 
 class Dog:
-	__slots__ = ('name','age')
+	__slots__ = ('name','age',"gender")
+
+
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
 	
 if __name__ == "__main__":
+	import json
 	whowork(Person())
-	whowork(Student('john',21))
-	whowork(Teacher())
+	#whowork(Student('john',21))
+	#whowork(Teacher())
 	whowork(Lawyer())
-	s = Student('john',22)
+	s = Student('john',22,'male')
 	s.gender = "male"
 	print(s.gender)
 	
@@ -69,7 +96,12 @@ if __name__ == "__main__":
 	d.age = 2
 	d.gender = 'male'
 	print(d.gender)
-	
+	print(s.score)
+	print(json.dumps(s,default=student2dict))
+	t = Teacher("Lee",35,'Professor')
+	print(json.dumps(t,default=lambda obj:obj.__dict__))
+	json_str = '{"name": "john", "age": 22, "gender": "male"}'
+	print(json.loads(json_str,object_hook=dict2student))
 	
 	
 	
