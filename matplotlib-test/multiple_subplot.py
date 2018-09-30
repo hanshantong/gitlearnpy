9e1d04b610fc4c72b524bb679d79ffaf2e25386e
hanshantong <1356466973@qq.com>
2018-09-30 16:55:42 +0800

    multiple subplot.

--- matplotlib-test/multiple_subplot.py
+++ matplotlib-test/multiple_subplot.py
@@ -3,6 +3,13 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib as mpl
+
+mpl.rc('lines', linewidth=1, color='b')
+
+#设置样式
+#plt.style.use(['dark_background', 'presention']) #组合样式
+plt.style.use('ggplot')
 
 x1 = np.linspace(0.0, 5.0)
 x2 = np.linspace(0.0, 2.0)
@@ -10,6 +17,11 @@ x2 = np.linspace(0.0, 2.0)
 y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
 y2 = np.cos(2 * np.pi * x2)
 
+'''
+with plt.style.context(('dark_background')):#限制style的范围：仅作用早subplot(2, 1, 1)
+	plt.subplot(2, 1, 1)
+	
+'''	
 plt.subplot(2, 1, 1)
 plt.plot(x1, y1, 'o-')
 
